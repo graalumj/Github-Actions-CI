2b0a252e8f181d60fc2d8ff5036ef1a81e473507
graalumj <graalumj@oregonstate.edu>
2021-06-06 11:55:51 -0700

    Added calculator.py and test.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# Calculator with Circle CI
+# By Alex Graalum
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
