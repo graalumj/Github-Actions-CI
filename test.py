0b9095550c649e4d8acb4a5e3cab9f0a3d6bb148
graalumj <graalumj@oregonstate.edu>
2021-06-06 12:01:58 -0700

    added multiply test and renamed previous tests

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ import calculator
 
 class TestCase(unittest.TestCase):
 
-    def test_add_1(self):
+    def test_add(self):
         self.assertEqual(calculator.add(1, 2), 3)
 
-    def test_subtract_1(self):
+    def test_subtract(self):
         self.assertEqual(calculator.subtract(1, 1), 0)
 
+    def test_mutliply(self):
+        self.assertEqual(calculator.multiply(2, 3), 6)
 
 if __name__ == '__main__':
     unittest.main()
