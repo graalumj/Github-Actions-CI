8232eab879ac802ab161fd53fb3d28eb98ae81af
graalumj <graalumj@oregonstate.edu>
2021-06-06 11:58:00 -0700

    Fixed test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import calculator
 class TestCase(unittest.TestCase):
 
     def test_add_1(self):
-        self.assertEqual(calculator.add(1,2), 3)
+        self.assertEqual(calculator.add(1, 2), 3)
 
 
 if __name__ == '__main__':
